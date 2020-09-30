f427d1c9d4b7450a40ee23969915c3e6ae38e85a
Alda Palma <alda.palma.luque@gmail.com>
2020-09-30 20:10:02 +0200

    color scheme changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { MapService } from './shared/map.service';
+import { Component, OnInit } from "@angular/core";
+import { MapService } from "./shared/map.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit{
-  title = 'cover-manager';
+export class AppComponent implements OnInit {
+  title = "cover-manager";
 
-  constructor(private mapService: MapService){}
+  constructor(private mapService: MapService) {}
 
-  ngOnInit(){
-
+  ngOnInit() {
     this.mapService.initFloors();
     this.mapService.initZones();
     this.mapService.initTables();
-    // this.mapService.initMap();
-
   }
 }
